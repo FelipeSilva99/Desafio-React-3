ae387ecce4dd7ccbcc13fee052c594edbca4976e
FelipeSilva99 <felipe.rl.silva@hotmail.com>
2021-06-05 13:20:15 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import  React, { Component } from 'react';
+import './App.css' 
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+class App extends Component{
+  state = {
+    nome: 'Felipe Silva',
+    idade: 22,
+    comidaF: 'Costela de porco grelhada',
+    fruta: 'minha fruta favorita se chama Pera',
+    musica: ['Froid - Flow de Lázaro', 'Filipe Ret - A.N.A.R.C.O.S', '6LACK - Prblms']
+  }
+  
+  render() {
+    return (
+      <div>
+      <h1>{this.state.nome}</h1>
+      <h2>{this.state.idade}</h2>
+      <h3>{this.state.comidaF}</h3>
+      <ul className='List'>
+      <li>{this.state.musica[0]}</li>
+      <li>{this.state.musica[1]}</li>
+      <li>{this.state.musica[2]}</li>
+      </ul>
+      <h4>{this.state.fruta}</h4>
+      <img src="/img/Pera.png" alt="Imagem da fruta Pera"/>
     </div>
-  );
+  )
+}
 }
 
 export default App;
